3cc5b4994c5101d49176971e83dbbff7e6441aba
Preport <erdemgezzz@gmail.com>
2024-03-06 18:28:40 +0300

    Create fsCache.test.ts

---
+++ src/schema/fsCache.test.ts
@@ -0,0 +1,50 @@
+import { FsCache } from './fsCache';
+import fs from 'fs';
+
+try {
+  fs.rmSync('./tmp/caches-test/', { recursive: true });
+} catch (err) {}
+let cache: FsCache;
+describe('FsCache', () => {
+  beforeAll(() => {
+    //@ts-expect-error private constructor
+    cache = new FsCache('./tmp/caches-test/');
+  });
+
+  it('should create a cache', async () => {
+    cache.set('test', 'test');
+  });
+
+  it('should get a cache', async () => {
+    await cache.set('test31', 'test');
+    const data = await cache.get('test31');
+    expect(data).toBe('test');
+  });
+
+  it('should fail to get a cache', async () => {
+    const data = await cache.get('test2');
+    expect(data).toBeUndefined();
+  });
+
+  it('should check if a key exists', async () => {
+    await cache.set('test66', 'test');
+    const exists = await cache.has!('test66');
+    expect(exists).toBe(true);
+  });
+
+  it('should remove a cache', async () => {
+    await cache.set('testdel', 'test');
+    await cache.delete('testdel');
+  });
+
+  it('should fail to remove a cache', async () => {
+    await cache.delete('test31');
+  });
+
+  it('should throw an error on getMany', () => {
+    expect(() => cache.getMany!(['test'])).toThrowError('Method not implemented.');
+  });
+  afterAll(() => {
+    cache.clear();
+  });
+});
