5b1d0045677c0557065fc60bf5492bc177b0c872
Preport <erdemgezzz@gmail.com>
2024-03-21 17:50:15 +0300

    Don't test getSchemaItems to reduce API calls

--- src/schema/builders/getSchemaItems.test.ts
+++ src/schema/builders/getSchemaItems.test.ts
@@ -1,6 +1,7 @@
 import { getSchemaItems } from './getSchemaItems';
 
 describe('getSchemaItems', () => {
+  /* Removed to reduce the number of requests to the API
   test('fetches schema items', async () => {
     const apiKey = process.env.API_KEY || '';
     const { items } = await getSchemaItems(apiKey);
@@ -10,7 +11,7 @@ describe('getSchemaItems', () => {
     expect(items.get(190)).toBe('Bat');
     expect(items.get('Bat')).toBe(0);
     await new Promise(resolve => setTimeout(resolve, 7000));
-  }, 15000);
+  }, 15000);*/
   test('fails to fetch the schema items with a bad api key', async () => {
     const apiKey = 'bad-api-key';
     await expect(getSchemaItems(apiKey)).rejects.toThrow('403');
