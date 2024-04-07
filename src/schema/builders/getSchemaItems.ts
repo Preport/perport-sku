7184a81593b1cdecbb38368c07d9b47dfe4548e1
Preport <erdemgezzz@gmail.com>
2024-04-08 00:33:25 +0300

    Cache GetSchemaItems based on last-modified header

--- src/schema/builders/getSchemaItems.ts
+++ src/schema/builders/getSchemaItems.ts
@@ -1,8 +1,17 @@
 import got from 'got';
 import { TwoWayMap } from '..';
 import { SpellMap } from '../mapExtensions';
+import fs from 'fs/promises';
 
-export async function getSchemaItems(apiKey: string) {
+export type GetSchemaItemsReturnType = Promise<{
+  items: TwoWayMap;
+  spells: SpellMap;
+  cosmeticParts: TwoWayMap;
+  parts: TwoWayMap;
+  upgradables: Map<string, number>;
+}>;
+
+export async function getSchemaItems(apiKey: string): GetSchemaItemsReturnType {
   const items = new Map() as TwoWayMap;
   const upgradables = new Map<string, number>();
   const spells = new SpellMap();
@@ -10,28 +19,42 @@ export async function getSchemaItems(apiKey: string) {
   const parts = new Map() as TwoWayMap;
   const cosmeticParts = new Map() as TwoWayMap;
   let resp: Result | undefined;
+
+  let useLocalCache = false;
+  const localLastChange = parseInt(await fs.readFile('./tmp/lastChange.txt', 'utf8').catch(() => '0'));
+  let remoteLastChange = 0;
   do {
-    resp = (
-      (await got
-        .get(
-          `https://api.steampowered.com/IEconItems_440/GetSchemaItems/v0001?key=${apiKey}&language=English&start=${
-            resp?.next || 0
-          }`,
-          {
-            //cache is broken for this endpoint
-            //cache: FsCache.get(),
-            retry: {
-              limit: 5,
-              // It is possible to get 404 from this endpoint claiming that it is retired
-              statusCodes: [404, 408, 413, 429, 500, 502, 503, 504, 521, 522, 524]
-            }
-          }
-        )
-        .json()) as /*.catch(err => {
-          console.log(err, Buffer.from(err.response.body).toString('utf8'), err.response?.isFromCache);
-          throw err;
-        })*/ GetSchemaItemsResponse
-    ).result;
+    const l = await got.get(
+      `https://api.steampowered.com/IEconItems_440/GetSchemaItems/v0001?key=${apiKey}&language=English&start=${
+        resp?.next || 0
+      }`,
+      {
+        //cache is broken for this endpoint
+        //cache: FsCache.get(),
+        retry: {
+          limit: 5,
+          // It is possible to get 404 from this endpoint claiming that it is retired
+          statusCodes: [404, 408, 413, 429, 500, 502, 503, 504, 521, 522, 524]
+        }
+      }
+    );
+    // Check only on the first request
+    if (!resp) {
+      const lastModified = l.headers['last-modified'];
+      if (lastModified !== undefined) {
+        const date = new Date(lastModified).getTime();
+        // Successful file access returns undefined
+        const fileAccess = await fs.access('./tmp/schemaItems.json', fs.constants.R_OK).catch(() => false);
+
+        if (date === localLastChange && fileAccess !== false) {
+          // read from cache
+          useLocalCache = true;
+          break;
+        }
+        remoteLastChange = date;
+      }
+    }
+    resp = (JSON.parse(l.body) as GetSchemaItemsResponse).result;
 
     for (const item of resp.items) {
       const name: string =
@@ -73,9 +96,31 @@ export async function getSchemaItems(apiKey: string) {
     await new Promise(resolve => setTimeout(resolve, 200));
   } while (resp.next !== undefined);
 
+  if (useLocalCache) {
+    return JSON.parse(await fs.readFile('./tmp/schemaItems.json', 'utf8'), JsonMapReviver);
+  }
+  const data = { items, spells, cosmeticParts, parts, upgradables };
+
+  await Promise.all([
+    fs.writeFile('./tmp/schemaItems.json', JSON.stringify(data, JsonMapReplacer)),
+    fs.writeFile('./tmp/lastChange.txt', remoteLastChange.toString())
+  ]);
+
   return { items, spells, cosmeticParts, parts, upgradables };
 }
 
+function JsonMapReplacer(_key: string, value: any) {
+  if (value instanceof Map) {
+    return { __map__: Array.from(value.entries()) };
+  }
+  return value;
+}
+function JsonMapReviver(_key: string, value: any) {
+  if (value && value.__map__) {
+    return new Map(value.__map__);
+  }
+  return value;
+}
 function getCounterID(attribs: Attribute[] | undefined) {
   return attribs?.find(attrib => attrib.class === 'strange_part_new_counter_id')?.value;
 }
